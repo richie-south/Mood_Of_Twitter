698c64303bfd87df8ad5c174e686e2fd0e6d042c
rs222kn <rs222kn@student.lnu.se>
2016-07-15 13:35:59 +0200

    hashHistory

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory} from 'react-router';
+import { Router, Route, IndexRoute, browserHistory, hashHistory} from 'react-router';
 
 import Layout from './pages/Layout';
 import TwitterMood from './pages/TwitterMood';
@@ -10,7 +10,7 @@ import MyTwitterMood from './pages/MyTwitterMood';
 
 const app = document.querySelector('#app');
 ReactDOM.render(
-    <Router history={browserHistory}>
+    <Router history={hashHistory}>
         <Route path="/" components={Layout}>
             <IndexRoute components={TwitterMood}></IndexRoute>
             <Route path="/twitterUserMood" components={MyTwitterMood}></Route>
